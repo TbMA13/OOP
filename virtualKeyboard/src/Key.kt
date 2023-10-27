64bb78147868962413e718d241d790f119277447
chandrianin <TbMA39@yandex.ru>
2023-10-28 00:22:09 +0200

    Добавление файлов

---
+++ virtualKeyboard/src/Key.kt
@@ -0,0 +1,3 @@
+package src
+
+data class Key(var keyName: String = "", var command: Command = Command())
---
+++ virtualKeyboard/src/Keyboard.kt
@@ -0,0 +1,24 @@
+package src
+
+
+class Keyboard {
+    private val keys = mutableSetOf<Key>()
+    private val lastActivity = mutableListOf<Key>()
+    private var brigthness = 0
+    private var volume = 0
+
+    fun addKey(newKey: Key) {
+    }
+
+    fun pressKey(targetKey: Key) {
+
+    }
+
+    fun addActivity(keyName: String, command: Command) {
+
+    }
+
+    fun undo() {
+
+    }
+}
