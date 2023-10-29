d37414a6a0f106b457bbe00a9c342c9345f3cda6
chandrianin <TbMA39@yandex.ru>
2023-10-29 14:36:48 +0200

    Демонстрация

--- virtualKeyboard/main.kt
+++ virtualKeyboard/main.kt
@@ -1,24 +1,28 @@
 package src.src
 
-import src.main.Key
-import src.main.Keyboard
-import src.main.StringCommand
-import src.main.VolumeUpCommand
+import src.main.*
 
 fun main() {
     val keyboard = Keyboard()
     val firstKey = Key("first", StringCommand())
     val secondKey = Key("second", StringCommand())
-    val thirdKey = Key("",  VolumeUpCommand())
+    val volumeKey = Key("",  VolumeUpCommand())
+    val brightnessKey = Key("",  BrightnessUpCommand())
     keyboard.addKey(firstKey)
     keyboard.addKey(secondKey)
-    keyboard.addKey(thirdKey)
+    keyboard.addKey(volumeKey)
+    keyboard.addKey(brightnessKey)
     keyboard.pressKey(firstKey)
     Thread.sleep(2000)
     keyboard.pressKey(secondKey)
     Thread.sleep(2000)
-    keyboard.pressKey(thirdKey)
+    keyboard.pressKey(volumeKey)
     Thread.sleep(2000)
+    keyboard.pressKey(brightnessKey)
+    Thread.sleep(2000)
+    keyboard.pressKey(volumeKey)
+    Thread.sleep(2000)
+
     keyboard.undo()
 
 }
